c61899cb3e61b4cba9c3a772d9a8ad881dc6c0c7
hyeeeonc <hyeeeonc@gmail.com>
2022-07-30 21:10:45 +0900

    그라데이션 제거

--- src/components/index/CarouselItem.tsx
+++ src/components/index/CarouselItem.tsx
@@ -2,16 +2,12 @@ import styled from '@emotion/styled'
 import React, { FunctionComponent } from 'react'
 
 const CarouselItem = styled.div`
-  box-sizing: border-box;
-
-  background: linear-gradient(
-    129.69deg,
-    #ffffff -110.73%,
-    rgba(255, 255, 255, 0) 98.01%
-  );
-  border: 3px solid #282828;
-  filter: drop-shadow(0px 8px 32px rgba(0, 0, 0, 0.25));
-  backdrop-filter: blur(300px);
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(3.5px);
+  -webkit-backdrop-filter: blur(3.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
   border-radius: 59px;
 
   flex: none;
--- src/components/index/MainCarousel2.tsx
+++ src/components/index/MainCarousel2.tsx
@@ -4,10 +4,6 @@ import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
 import CarouselItems from './CarouselItem'
 
 const CarouselBlock = styled.div`
-  position: absolute;
-  top: 56px;
-  left: 0;
-  // width: calc( 1016px*7 + (50vw - 508px - 200px) * 6 + 50vw - 508px)
   height: 610px;
   display: flex;
   align-items: center;
@@ -22,48 +18,37 @@ const MainCarousel2: FunctionComponent = function () {
   const carouselBlock = useRef<HTMLDivElement>(null)
   const [carouselScroll, setCarouselScroll] = useState<number>(0)
 
-  // let slider = carouselBlock.current
-  let sliderWidth
-  let current = 0
-  let target = 0
-  const ease = 0.3
-
-  const setter = (start: number, end: number, t: number): number => {
-    return start * (1 - t) + end * t
-  }
-
-  // const setTransform = (el, transform) => {
-  //   el.style.transform = transform
-  // }
-
-  const init = () => {
-    sliderWidth = carouselBlock.current.getBoundingClientRect().width
-    document.body.style.height = `${
-      sliderWidth - (window.innerWidth - window.innerHeight)
-    }px`
-  }
-
-  const animate = () => {
-    current = Math.round(setter(current, target, ease) * 1e2) / 1e2
-    target = window.scrollY
-    setCarouselScroll(current)
-    // console.log(current)
-    requestAnimationFrame(animate)
-  }
-
   useEffect(() => {
+    let sliderWidth
+    let current = 0
+    let target = 0
+    const ease = 0.3
+
+    const setter = (start: number, end: number, t: number): number => {
+      return start * (1 - t) + end * t
+    }
+
+    const init = () => {
+      sliderWidth = carouselBlock.current.getBoundingClientRect().width
+      document.body.style.height = `${
+        sliderWidth - (window.innerWidth - window.innerHeight)
+      }px`
+    }
+
+    const animate = () => {
+      current = Math.round(setter(current, target, ease) * 1e2) / 1e2
+      target = window.scrollY
+      setCarouselScroll(current)
+      requestAnimationFrame(animate)
+    }
     init()
     animate()
-    console.log(carouselScroll)
   }, [])
-
-  useEffect(() => {
-    console.log(carouselScroll)
-  }, [carouselScroll])
 
   return (
     <CarouselBlock
       ref={carouselBlock}
+      className="slider"
       style={{ transform: `translateX(-${carouselScroll}px)` }}
     >
       <CarouselWrapper>
